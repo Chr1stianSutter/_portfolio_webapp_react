2230af104a8108bb05110a4f503d310aaf6ca987
Wenzel Massag <mail@wmassag.de>
2019-06-09 21:03:04 +0200

    Removed clutter

--- src/js/pages/Projects/Project1/Project1.js
+++ src/js/pages/Projects/Project1/Project1.js
@@ -6,534 +6,80 @@ import "./style.scss"
 
 export default class Project1 extends React.Component {
   render() {
-
-    const bigTextStyle={
-      //fontSize:"90pt",
-      fontFamily: 'Montserrat',
-      textTransform:"uppercase",
-      lineHeight:"0.9em",
-      //position: "absolute",
-      //marginLeft:"20px"
-    };
-
-    const gridStyle = {
-      //backgroundColor:"red"
-    }
-
-    const bigTextStyleGuideStyle={
-      //fontSize:"90pt",
-      fontFamily: 'Montserrat',
-      textTransform:"uppercase",
-      lineHeight:"0.9em",
-      //position: "absolute",
-      //marginTop: "200px"
-    };
-
-    const bigTextNavigationStyle={
-      //fontSize:"90pt",
-      fontFamily: 'Montserrat',
-      textTransform:"uppercase",
-      lineHeight:"0.9em",
-      //position: "absolute",
-      //marginTop: "120px",
-      //marginBottom:"50px"
-    };
-
-    const bigTextUsersStyle={
-      //fontSize:"90pt",
-      fontFamily: 'Montserrat',
-      textTransform:"uppercase",
-      lineHeight:"0.9em",
-      //position: "absolute",
-      //marginTop: "120px",
-      textAlign: "right",
-
-    };
-
-    const smallTextStyle ={
-      fontFamily: 'Montserrat',
-      //fontSize:"20pt"
-    };
-
-    const smallTextStyleRight ={
-      fontFamily: 'Montserrat',
-      //fontSize:"20pt",
-      textAlign:"right"
-    };
-
-    const mottoStyleRight ={
-      fontFamily: 'Montserrat',
-      //fontSize:"48pt",
-      textAlign:"left",
-      //position:"absolute",
-      //width: "100%",
-      //marginTop:"350px",
-      //marginRight:"0px",
-      //marginLeft:"450px"
-
-    };
-
-    const smallTextStyleFloat ={
-      fontFamily: 'Montserrat',
-      //fontSize:"18pt",
-      textTransform:"none",
-      //position:"absoulte"
-    };
-
-    const smallTextStyleFloatRight ={
-      fontFamily: 'Montserrat',
-      //fontSize:"18pt",
-      textTransform:"none",
-      //position:"absoulte",
-      textAlign:"right"
-    };
-
-    const smallTextStyleFloatLeft ={
-      fontFamily: 'Montserrat',
-      //fontSize:"18pt",
-      textTransform:"none",
-      //position:"absoulte",
-      textAlign:"left"
-    };
-
-
-    const smallTextStyleTypefaceA ={
-      fontFamily: 'Montserrat',
-      //fontSize:"18pt",
-      textTransform:"none"
-    };
-
-
-    const divBarStyle = {
-      height:"10px",
-      width:"150px",
-      //marginTop: "30px",
-      //marginBottom:"25px",
-      backgroundColor:'black'
-    }
-
-    const divBarStyleRight = {
-      height:"10px",
-      width:"150px",
-      //marginTop: "30px",
-      //marginBottom:"25px",
-      backgroundColor:'black',
-      //marginLeft:"auto",
-      //marginRight:"0",
-      //float:"right"
-    }
-
-    const cardImageStyle = {
-      //height: "600px",
-      //width: "100%",
-      //marginTop:"65px"
-    };
-
-    const cardImageStyleTwoSmall = {
-      //height: "400px",
-      //width: "100%",
-      //marginTop:"65px"
-    };
-
-    const hugeBgTextStyle = {
-      //fontSize:"240pt",
-      fontFamily: 'Montserrat',
-      textTransform:"uppercase",
-      lineHeight:"0.9em",
-      letterSpacing: "0.05em",
-      color: "gray",
-      opacity: "0.1",
-      //marginBottom: "350px"
-    }
-
-    const hugeBgTextStyleNavigation = {
-      //fontSize:"240pt",
-      fontFamily: 'Montserrat',
-      textTransform:"uppercase",
-      lineHeight:"0.9em",
-      letterSpacing: "0.05em",
-      color: "gray",
-      opacity: "0.1",
-      //marginBottom: "50px"
-    }
-
-
-    const hugeBgTextStyleRight = {
-      //fontSize:"240pt",
-      fontFamily: 'Montserrat',
-      textTransform:"uppercase",
-      lineHeight:"0.9em",
-      letterSpacing: "0.05em",
-      color: "gray",
-      opacity: "0.1",
-      textAlign: "right",
-      //marginBottom: "650px"
-    }
-
-    const absoluteStyle = {
-      //position:"absolute",
-      textAlign: "right",
-      //width: "98%",
-      //marginRight:"20px"
-    };
-
-    const wrapperStyle = {
-      width: "100%",
-    }
-    const ulSyle = {
-      direction:"rtl"
-    }
-
-    const spacerDiv = {
-      height: "50px"
-    };
-
-    const insetStyle = {
-      //marginTop:"30px",
-      //marginRight: "10px",
-      //marginBottom: "70px",
-      inlineStyle:"block",
-      float: "left",
-      shapeOutside:"inset(0px)",
-      clipPath:"inset(0px)",
-      //width:"150px",
-      //height:"150px",
-      backgroundColor:"black"
-    }
-
     return(
       <div>
-        <Grid columns='equal' >
-          <Grid.Column>
-            <div class="column" />
-          </Grid.Column>
-
-          <Grid.Column width={8} style = {gridStyle}>
-            <div class="eight wide column">
-              <h2 style={smallTextStyle}>
-                {"Projects:"}
-              </h2>
-
-              <h1 style={bigTextStyle}>
-                {"SEGWAY"}<br/>
-                {"UI"}
-                <div style={divBarStyle}></div>
-                <h2 style={smallTextStyleFloat}>
-                  {"A tablet interface mounted on"}<br/>
-                  {"a segway to provide the user"}<br/>
-                  {"with a multitude of features"}
-                </h2>
-              </h1>
-              <img src={image} style={cardImageStyle}/>
-              {/*<div style={spacerDiv} />*/}
-            </div>
-
-            <Grid.Column>
-              <div class="column" />
-            </Grid.Column>
-
-            <Grid columns='equal'>
-              <Grid.Column width={8}>
-                <div class="eight wide column">
-                    <img src={image} style={cardImageStyle}/>
-                </div>
-              </Grid.Column>
-              <Grid.Column width={8}>
-                <div class="four wide column">
-                  <h1 style={bigTextStyle}>
-                    {"*NAME*"}
-                    <div style={divBarStyle} />
-                    <h2 style={smallTextStyleFloat}>
-                    {"Lorem ipsum dolor sit amet, consetetur"}<br/>
-                    {"sadipscing elitr, sed diam nonumy"}<br/>
-                    {"eirmod tempor invidunt ut labore et"}<br/>
-                    {"dolore magna aliquyam erat."}<br/>
-                    <br/>
-                    {"At vero eos et accusam et justo duo"}<br/>
-                    {"dolores et ea rebum. Stet clita kasd"}<br/>
-                    {"gubergren."}"</h2>
-                  </h1>
-                </div>
-              </Grid.Column>
-            </Grid>
-          </Grid.Column>
-
-          <Grid.Column>
-            <div class="column" />
-          </Grid.Column>
-        </Grid>
-
-        <Grid style = {gridStyle}>
-          <Grid.Column width={4} />
-          <Grid.Column width={8}>
-            <h2 style={bigTextStyleGuideStyle}>
-              {"Style Guide"}
-              <div style={divBarStyle} />
-
-              <h2 style = {smallTextStyle}>
-                {"Color"}
-              </h2>
-
-              <Grid.Column>
-                <div style={insetStyle} />
-                <div style={insetStyle} />
-                <div style={insetStyle} />
-                <div style={insetStyle} />
-                <div style={insetStyle} />
-              </Grid.Column>
-
-              <h2 style = {smallTextStyle}>
-                {"Typefaces"}
-              </h2>
-
-              <Grid columns='equal' >
-                <Grid.Column width={8} style = {gridStyle}>
-                  <h2 style = {smallTextStyleFloat}>
-                    {"Futura - Bold"}
-                    <div style={divBarStyle} />
-                    {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"}<br/>
-                    {"abcdefghijklmnopqrstuvwxyz"}<br/>
-                    {"0123456789"}
-                  </h2>
-                </Grid.Column>
-                <Grid.Column width={8}>
-                  <h2 style = {smallTextStyleFloat}>
-                    {"Helvetica - Regular"}
-                  <div style={divBarStyle} />
-                    {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"}<br/>
-                    {"abcdefghijklmnopqrstuvwxyz"}<br/>
-                    {"0123456789"}
-                  </h2>
-                </Grid.Column>
-              </Grid>
-
-            </h2>
-          </Grid.Column>
-        </Grid>
-
-        <Grid>
-          <Grid.Column width={2} />
-          <Grid.Column width={8}>
-            <h1 style={hugeBgTextStyle}>
-              {"Style"}<br/>
-              {"Guide"}
-            </h1>
-          </Grid.Column>
-        </Grid>
-
-        <Grid>
-          <Grid.Column width={4} />
-          <Grid.Column width={8} style = {gridStyle}>
-            <div style = {absoluteStyle}>
-              <h2 style={bigTextUsersStyle}>
-                {"Users"}
-              </h2>
-              <div style={divBarStyleRight}></div>
-
-              <Grid columns='equal'>
-                <Grid.Column width={8}>
-                  <img src={image} style={cardImageStyle}/>
-                </Grid.Column>
-
-                <Grid.Column width={8}>
-                  <h2 style = {smallTextStyleRight}>
-                    {"Needs"}
-                  </h2>
-                  <h1 style={smallTextStyleRight}>
-                    <ul style = {ulSyle}>
-                      <li>
-                        {"test 1"}
-                      </li>
-                      <li>
-                        {"test 2"}
-                      </li>
-                      <li>
-                        {"test 3"}
-                      </li>
-                      <li>
-                        {"test 4"}
-                      </li>
-                    </ul>
-                  </h1>
-                  <h2 style = {smallTextStyleRight}>
-                    {"Motto"}
-                  </h2>
-                </Grid.Column>
-                <h2 style = {mottoStyleRight}>
-                  {"I want to make use of"}<br/>
-                  {"positive affirmations to"}<br/>
-                  {"further my growth!"}
-                </h2>
-              </Grid>
-            </div>
-          </Grid.Column>
-        </Grid>
-
-        <Grid>
-          <Grid.Column width={6} />
-          <Grid.Column width={8}>
-            <h1 style={hugeBgTextStyleRight}>
-              {"Users"}
-            </h1>
-          </Grid.Column>
-        </Grid>
-
-        <Grid style = {gridStyle}>
-          <Grid.Column width={4}></Grid.Column>
-          <Grid.Column width={8}>
-            <h2 style={bigTextNavigationStyle}>
-              {"Navigation"}
-              <div style={divBarStyle} />
-              <h2 style = {smallTextStyle}>
-                {"*Title*"}
-              </h2>
-            </h2>
-          </Grid.Column>
-        </Grid>
-        <Grid>
-          <Grid.Column width={2} />
-          <Grid.Column width={8}>
-            <h1 style={hugeBgTextStyleNavigation}>
-              {"Navigation"}
-            </h1>
-          </Grid.Column>
-        </Grid>
-
-        <Grid columns='equal'>
-          <Grid.Column>
-            <div class="column" />
-          </Grid.Column>
-          <Grid.Column wid th={8}>
-            <Grid columns='equal'>
-              <Grid.Column width={8}>
-              <div class="eight wide column">
-                <h2 style={smallTextStyleFloatRight}>
-                  {"Lorem ipsum dolor sit amet, consetetur"}<br/>
-                  {"sadipscing elitr, sed diam nonumy"}<br/>
-                  {"eirmod tempor invidunt ut labore et"}<br/>
-                  {"dolore magna aliquyam erat."}
-                  <br/>
-                  <br/>
-                  <br/>
-                  <br/>
-                </h2>
-                <img src={image} style={cardImageStyleTwoSmall}/>
-              </div>
-              </Grid.Column>
-              <Grid.Column width={8}>
-                <div class="four wide column">
-                  <img src={image} style={cardImageStyleTwoSmall}/>
-                  <h2 style={smallTextStyleFloat}>
-                    <br/>
-                    <br/>
-                    <br/>
-                    {"Lorem ipsum dolor sit amet, consetetur"}<br/>
-                    {"sadipscing elitr, sed diam nonumy"}<br/>
-                    {"eirmod tempor invidunt ut labore et"}<br/>
-                    {"dolore magna aliquyam erat."}
-                  </h2>
-                </div>
-              </Grid.Column>
-            </Grid>
-          </Grid.Column>
-          <Grid.Column>
-            <div class="column"></div>
-          </Grid.Column>
-        </Grid>
-        {/*<div style={spacerDiv} />*/}
-        {/*<div style={spacerDiv} />*/}
-        {/*<div style={spacerDiv} />*/}
-        <Grid>
-          <Grid.Column width={4} />
-          <Grid.Column width={8} style = {gridStyle}>
-            <div style = {absoluteStyle}>
-              <h2 style={bigTextUsersStyle}>
-                {"Music"}
-              </h2>
-              <div style={divBarStyleRight} />
-              <h2 style = {smallTextStyleRight}>
-                {"Play your favorite songs!"}
-              </h2>
-              <div style={spacerDiv} />
-              <Grid columns='equal'>
-                <Grid.Column width={8}>
-                  <img src={Image} style={cardImageStyleTwoSmall}/>
-                  <h2 style={smallTextStyleFloat}>
-                    <br/>
-                    <br/>
-                    {"Lorem ipsum dolor sit amet, consetetur"}<br/>
-                    {"sadipscing elitr, sed diam nonumy"}<br/>
-                    {"eirmod tempor invidunt ut labore et"}<br/>
-                    {"dolore magna aliquyam erat."}
-                  </h2>
-                </Grid.Column>
-                <Grid.Column width={8}>
-                  <h2 style={smallTextStyleFloatLeft}>
-                    {"Lorem ipsum dolor sit amet, consetetur"}<br/>
-                    {"sadipscing elitr, sed diam nonumy"}<br/>
-                    {"eirmod tempor invidunt ut labore et"}<br/>
-                    {"dolore magna aliquyam erat."}
-                    <br/>
-                    <br/>
-                    <br/>
-                  </h2>
-                  <img src={image} style={cardImageStyleTwoSmall}/>
-                </Grid.Column>
-              </Grid>
-            </div>
-          </Grid.Column>
-        </Grid>
-
-        <Grid>
-        <Grid.Column width={6} />
-          <Grid.Column width={8}>
-            <h1 style={hugeBgTextStyleRight}>
-              {"Music"}
-            </h1>
-          </Grid.Column>
-        </Grid>
-        {/*<div style={spacerDiv} />*/}
-        {/*<div style={spacerDiv} />*/}
-        {/*<div style={spacerDiv} />*/}
-
-        <Grid style = {gridStyle}>
-          <Grid.Column width={4} />
-          <Grid.Column width={8}>
-            <h2 style={bigTextNavigationStyle}>
-              {"*Name*"}
-              <div style={divBarStyle} />
-              <h2 style = {smallTextStyle}>
-                {"A stylish and green way of"}
-                <br/>
-                {"transportation!"}
-              </h2>
-            </h2>
-          </Grid.Column>
-        </Grid>
-
-        <Grid>
-          <Grid.Column width={2} />
-          <Grid.Column width={8}>
-            <h1 style={hugeBgTextStyleNavigation}>
-              {"*Name*"}
-            </h1>
-          </Grid.Column>
-        </Grid>
-
-        <Grid columns='equal' >
-          <Grid.Column>
-            <div class="column" />
-          </Grid.Column>
-          <Grid.Column width={8} style = {gridStyle}>
-            <div class="eight wide column">
-              <img src={image} style={cardImageStyle}/>
-            </div>
-          </Grid.Column>
-          <Grid.Column>
-            <div class="column" />
-          </Grid.Column>
-        </Grid>
-
+        <h2>{"Projects:"}</h2>
+        <h1>{"SEGWAY UI"}</h1>
+        <blockquote>
+          {"A tablet interface mounted on a segway to provide the user with a multitude of features"}
+        </blockquote>
+        <img src={image} />
+        <h1>{"*NAME*"}</h1>
+        <blockquote>
+          {"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren."}
+        </blockquote>
+        <h2>{"Style Guide"}</h2>
+        <h2>{"Color"}</h2>
+        <h2>{"Typefaces"}</h2>
+        <h2>
+          {"Futura - Bold"}
+          {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"}<br/>
+          {"abcdefghijklmnopqrstuvwxyz"}<br/>
+          {"0123456789"}
+        </h2>
+        <h2>
+          {"Helvetica - Regular"}
+          {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"}<br/>
+          {"abcdefghijklmnopqrstuvwxyz"}<br/>
+          {"0123456789"}
+        </h2>
+        <h1>
+          {"Style"}<br/>
+          {"Guide"}
+        </h1>
+        <h2>
+          {"Users"}
+        </h2>
+        <h2>
+          {"Needs"}
+        </h2>
+        <ul>
+          <li>{"test 1"}</li>
+          <li>{"test 2"}</li>
+          <li>{"test 3"}</li>
+          <li>{"test 4"}</li>
+        </ul>
+        <h2>{"Motto"}</h2>
+        <blockquote>
+          {"I want to make use of positive affirmations tofurther my growth!"}
+        </blockquote>
+        <h1>{"Users"}</h1>
+        <h2>{"Navigation"}</h2>
+        <h2>{"*Title*"}</h2>
+        <h1>{"Navigation"}</h1>
+        <blockquote>
+          {"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat."}
+        </blockquote>
+        <img src={image} />
+        <img src={image}/>
+        <blockquote>
+          {"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat."}
+        </blockquote>
+        <h2>{"Music"}</h2>
+        <h2>{"Play your favorite songs!"}</h2>
+        <img src={Image} />
+        <blockquote>
+          {"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat."}
+        </blockquote>
+        <blockquote>
+          {"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat."}
+        </blockquote>
+        <img src={image} />
+        <h1>{"Music"}</h1>
+        <h2>{"*Name*"}</h2>
+        <blockquote>{"A stylish and green way of transportation!"}</blockquote>
+        <h1>{"*Name*"}</h1>
+        <img src={image} />
       </div>
     );
   }
